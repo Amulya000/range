fe94cf9de24fc6ea69f5878681867cea2c33ec24
Amulya000 <125048010+Amulya000@users.noreply.github.com>
2023-02-27 21:25:04 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,42 @@
+# a=1,2,3,4,5
+# print(*a)
+
+# a=range(10)
+# print(a)
+
+# a=10,20,30
+# for am in a:
+    # print(am)
+
+# a=10,20,30
+# for p in a:
+#     print(p, end=' ')
+
+
+# for i in range(5):
+#     print(i)
+
+# n=int(input('n:'))
+# for i in range(1,n+1):
+#     print(i,end="\n")
+
+# for i in range(1,30,2):
+#         print(i)
+
+
+# for i in range(10,0,-2):
+#         print(i)
+
+# n=int(input('enter n value:'))
+# for i in range(1,n+2):
+#     print(i)
+
+# l=list("1234")
+# l[0]=l[1]=5
+# print(l)
+
+# for num in range(-2,-5,-1):
+#     print(num,end=", ")
+
+# for num in range(-2,5,1):
+#     print(num,end=", ")
